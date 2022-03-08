8f69d96c9d0b3c751b3b1cb37d8dcf04af18c8b4
vitasika <vitasika1@gmail.com>
2022-03-08 10:40:58 +0200

    добавил рандомное отображение цветов (формула)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,3 +9,16 @@ console.log(btnStopEl);
 // Нахидим общий body на странице для смены на нем цвета фона
 const bodyEl = document.querySelector('body');
 console.log(bodyEl);
+
+
+
+
+
+
+
+/*
+Для генерации случайного цвета используй функцию getRandomHexColor.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+*/
