a0f0b78dd4d96df8dcae86e455fa5a1523a35bfb
vitasika <vitasika1@gmail.com>
2022-03-08 10:37:31 +0200

    начал 9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+// Найти строку в html кнопку Start с названием data-start
+const btnStartEl = document.querySelector('[data-start]');
+console.log(btnStartEl);
 
+// Найти строку в html кнопку Stop с названием data-stop
+const btnStopEl = document.querySelector('[data-stop]');
+console.log(btnStopEl);
+
+// Нахидим общий body на странице для смены на нем цвета фона
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
