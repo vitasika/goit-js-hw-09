d78bf75298668496791e3ada25471bae31dd99d1
vitasika <vitasika1@gmail.com>
2022-04-02 10:10:46 +0300

    слушатели событий на кнопках

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,19 +6,21 @@ console.log(btnStartEl);
 const btnStopEl = document.querySelector('[data-stop]');
 console.log(btnStopEl);
 
-// Нахидим общий body на странице для смены на нем цвета фона
+// Находим общий body на странице для смены на нем цвета фона
 const bodyEl = document.querySelector('body');
 console.log(bodyEl);
 
+// Вызываем слушателя события 'click' на кнапке btnStartEl (start) которая вызывавет колбэк функцию onBtnStartClick
+btnStartEl.addEventListener('click', onBtnStartClick)
+
+// Вызываем слушателя события 'click' на кнапке btnStopEl (stop) которая вызывавет колбэк функцию onBtnStopClick
+btnStopEl.addEventListener('click', onBtnStopClick)
 
 
 
 
 
-
-/*
-Для генерации случайного цвета используй функцию getRandomHexColor.
+//Для генерации случайного цвета используем функцию getRandomHexColor по условию задачи.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-*/
