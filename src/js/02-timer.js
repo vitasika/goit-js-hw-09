477604839e41bcd7516a8315b1256c5ee50d6eb3
vitasika <vitasika1@gmail.com>
2022-04-02 13:28:46 +0300

    нахожу нужные строки в html

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,3 +5,38 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 // all modules
 import Notiflix from 'notiflix';
+
+/* Выбираем елементы 
+input, 
+button, 
+span[data - days], 
+span[data - hours],
+span[data - minutes], 
+span[data - seconds]
+*/
+
+const inputEl = document.querySelector('#datetime-picker');
+console.log('#datetime-picker');
+
+const startEl = document.querySelector('button[data-start]');
+console.log('button[data-start]');
+
+// Объект параметров (из ТЗ)
+const options = {
+    // Включает выбор времени
+    enableTime: true, 
+
+    // Отображает средство выбора времени в 24-часовом режиме без выбора AM/PM, если включено.
+    time_24hr: true,
+
+    // Устанавливает начальную выбранную дату (даты)
+    defaultDate: new Date(),
+
+    // Регулирует шаг ввода минут (включая прокрутку)
+    minuteIncrement: 1,
+
+    // Функция(и) для запуска при каждом закрытии календаря
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    }, 
+};
