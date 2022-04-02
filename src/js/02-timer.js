28fd3173e816956962be015749e7d7e52e55d3c8
vitasika <vitasika1@gmail.com>
2022-04-02 12:36:42 +0300

    подключил и импортировал flatpickr и Notiflix

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+// all modules
+import Notiflix from 'notiflix';
