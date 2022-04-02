f1dcc7e15d99b462fb542c4950042668d7e307d5
vitasika <vitasika1@gmail.com>
2022-04-02 14:13:56 +0300

    9_2_1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -21,6 +21,18 @@ console.log('#datetime-picker');
 const startEl = document.querySelector('button[data-start]');
 console.log('button[data-start]');
 
+const daysEl = document.querySelector('span[data-days]');
+console.log('span[data-days]');
+
+const hoursEl = document.querySelector('span[data-hours]');
+console.log('span[data-hours]');
+
+const minutesEl = document.querySelector('span[data-minutes]');
+console.log('span[data-minutes]');
+
+const secondsEl = document.querySelector('span[data-seconds]');
+console.log('span[data-seconds]');
+
 // Объект параметров (из ТЗ)
 const options = {
     // Включает выбор времени
